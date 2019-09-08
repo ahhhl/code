face156be8ef34ee0d4ab6bc0415769137b0f6e4
ahh <1208484386@qq.com>
2019-09-08 15:46:53 +0800

    心理测评选择题

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
